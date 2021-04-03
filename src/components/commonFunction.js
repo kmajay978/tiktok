78aba55b9b609b66dcad818ab7d8480082d69862
Dupinder Singh <dupinder.15c@gmail.com>
2021-04-03 18:58:27 +0530

    add page and html

--- src/components/commonFunction.js
+++ src/components/commonFunction.js
@@ -1,32 +1,3 @@
 import React from 'react';
 import $ from 'jquery';
 
-// export const sideMenuJs = () => {
-//     var header_height = $(".top_bar").innerHeight();
-
-//     $(".side_menu").css({
-//         "top": header_height
-//     });
-
-//     $(".menu").on("click", function () {
-//         $(".side_menu").toggleClass("active");
-//         return false;
-//     });
-
-//     $("html").on("click", function () {
-//         $(".side_menu").removeClass("active");
-//     });
-//     $(".menu, .side_menu").on("click", function (e) {
-//         e.stopPropagation();
-//     });
-
-//     $(".user-log").on("click", function () {
-//         $(".account-menu").slideToggle();
-//     });
-//     $("html").on("click", function () {
-//         $(".account-menu").slideUp();
-//     });
-//     $(".user-log, .account-menu").on("click", function (e) {
-//         e.stopPropagation();
-//     });
-// }
